c409ee53f93fc7440da6655ab90025353512b3f8
paweldomas <pawel.domas@jitsi.org>
2020-02-26 13:54:05 -0600

    extract MockServer

--- tests/main.js
+++ tests/main.js
@@ -1,17 +1,14 @@
 require.config({
-    baseUrl: '../',
+    baseUrl: './',
     paths: {
-        "strophe":          "node_modules/strophe.js/dist/strophe.umd",
-        "websocket":        "node_modules/mock-socket/dist/mock-socket",
-        "streammanagement": "lib/strophe.stream-management",
-
-        // Tests
-        "jquery":           "node_modules/jquery/dist/jquery",
-        "tests":            "tests/tests"
+        "strophe":          "../node_modules/strophe.js/dist/strophe.umd",
+        "websocket":        "../node_modules/mock-socket/dist/mock-socket",
+        "streammanagement": "../lib/strophe.stream-management",
+        "jquery":           "../node_modules/jquery/dist/jquery"
     }
 });
 
-require(["tests", "strophe.js", "websocket", "streammanagement"], function (tests, Strophe, mockWebsocket) {
+require(["tests"], function (tests) {
     QUnit.done(function (details) {
         console.log("Total: "+details.total+" Failed: "+details.failed+" Passed: "+details.passed+" Runtime: "+details.runtime);
         console.log("All tests completed!");
--- tests/tests.js
+++ tests/tests.js
@@ -3,8 +3,9 @@
 define([
     'jquery',
     'strophe.js',
-    'websocket'
-    ], function($, wrapper, websocket) {
+    'streammanagement',
+    'MockServer'
+    ], function($, wrapper, streammanagement, MockServer) {
     const Strophe = wrapper.Strophe;
     const XMPP_DOMAIN = 'anonymous.server.com';
     const WEBSOCKET_URL = 'ws://localhost:8888';
@@ -61,77 +62,11 @@ define([
         ];
     }
 
-    class MockServer {
-        constructor({ assert, wsUrl, responseStreams }) {
-            const Server = websocket.Server;
-
-            this.mockServer = new Server(wsUrl);
-            let socketCounter = 0;
-
-            this.mockServer.on('connection', socket => {
-                const socketIdx = socketCounter;
-
-                socketCounter += 1;
-
-                socket.respIdx = 0;
-                let processedStanzas = 0;
-                socket.on('message', msg => {
-                    console.info('Server received:', msg);
-                    let responseStream = responseStreams[socketIdx];
-
-                    if (!responseStream) {
-                        socket.close(1000, 'unexpected connection');
-
-                        return;
-                    }
-
-                    let response;
-                    if (msg.startsWith('<r xmlns="urn:xmpp:sm:3"')) {
-                        response = `<a xmlns="urn:xmpp:sm:3" h="${processedStanzas}" />`;
-                    } else {
-                        response = responseStream[socket.respIdx];
-                        socket.respIdx += 1;
-                    }
-
-                    console.info('Server response:', response);
-
-                    if (Array.isArray(response)) {
-                        for(const r of response) {
-                            socket.send(r);
-                        }
-                    } else if (response) {
-                        if (response === 'result') {
-                            processedStanzas += 1;
-                            const id = msg.match(' id="(.*):sendIQ" ')[1];
-                            response = `<iq type='result' from='${XMPP_DOMAIN}' to='${JID}' xmlns='jabber:client' id='${id}:sendIQ'/>`
-                        } else if (response === 'ignore') {
-
-                            return;
-                        } else if (response === 'close') {
-                            socket.close();
-
-                            return;
-                        }
-                        socket.send(response);
-                    } else {
-                        assert.ok(false, "Unexpected msg received: " + msg);
-                        socket.close();
-                    }
-                })
-            });
-        }
-
-        cleanup() {
-            for(const client of this.mockServer.clients()) {
-                client.close(1000, 'test cleanup');
-            }
-            this.mockServer.stop();
-        }
-    }
-
     function createTestPromise(mockServerOptions, executor) {
         const mockServer = new MockServer({
             wsUrl: WEBSOCKET_URL,
+            xmppDomain: XMPP_DOMAIN,
+            userJid: JID,
             ...mockServerOptions
         });
 
